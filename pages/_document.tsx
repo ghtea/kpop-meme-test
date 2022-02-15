8465323e367e5b20e6b5c64ddf0774bcee5a8d96
jaehyeon(wiz) <eiirwp@gmail.com>
2022-02-15 23:20:53 +0900

    WIP add google analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+// https://mariestarck.com/add-google-analytics-to-your-next-js-application-in-5-easy-steps/
+// https://analytics.google.com/analytics/web/#/a220434685p303648469/admin/streams/table/3248083197
+
+import Document, {DocumentContext, Head, Html, Main, NextScript} from "next/document"
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
