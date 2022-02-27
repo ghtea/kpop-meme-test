aec75fe84e694eac197c4bce483c87451190ea86
jaehyeon(wiz) <eiirwp@gmail.com>
2022-02-27 12:16:05 +0900

    fix replace static to server side

--- pages/tests/[testId]/result/index.tsx
+++ pages/tests/[testId]/result/index.tsx
@@ -1,6 +1,6 @@
 import {ParsedUrlQuery} from "querystring";
 import copy from "copy-to-clipboard";
-import type {GetStaticPaths, GetStaticProps, NextPage} from "next"
+import type {GetServerSideProps, GetStaticPaths, GetStaticProps, NextPage} from "next"
 import {NextSeo} from "next-seo";
 import {useRouter} from "next/router"
 import {useCallback, useEffect, useMemo, useState} from "react"
@@ -239,33 +239,52 @@ const ResultPage: NextPage<ResultPageProps> = ({
   )
 }
 
-export const getStaticPaths: GetStaticPaths = () => {
-  const scoreParams = [0,1,2,3,4,5,6,7,8,9,10].map(item => encrypt(item.toString()))
-  const testIdParams = ["food", "knowledge"]
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const scoreParams = [0,1,2,3,4,5,6,7,8,9,10].map(item => encrypt(item.toString()))
+//   const testIdParams = ["food", "knowledge"]
 
-  const paths: (string | {
-    params: ParsedUrlQuery;
-    locale?: string | undefined;
-  })[] = []
+//   const paths: (string | {
+//     params: ParsedUrlQuery;
+//     locale?: string | undefined;
+//   })[] = []
 
-  scoreParams.forEach(scoreParam =>{
-    testIdParams.forEach(testIdParam => {
-      const params = {
-        testId: testIdParam,
-        s: scoreParam
-      }
+//   scoreParams.forEach(scoreParam =>{
+//     testIdParams.forEach(testIdParam => {
+//       const params = {
+//         testId: testIdParam,
+//         s: scoreParam
+//       }
 
-      paths.push({params})
-    })
-  })
+//       paths.push({params})
+//     })
+//   })
+//   console.log("paths: ", paths); // TODO: remove
 
-  return {
-    paths,
-    fallback: true,
-  };
-}
+//   return {
+//     paths,
+//     fallback: true,
+//   };
+// }
 
-export const getStaticProps: GetStaticProps = (context) => {
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   const testId = context?.params?.testId || ""
+//   console.log("testId: ", testId); // TODO: remove
+//   const scoreParam = context?.params?.s || ""
+
+//   const encryptedScore = typeof scoreParam === "string" ? scoreParam : ""
+//   console.log("encryptedScore: ", encryptedScore); // TODO: remove
+//   const decryptedScore = encryptedScore ? parseInt(decrypt(encryptedScore), 10) : -1
+
+//   console.log("defaultScore: ", decryptedScore); // TODO: remove
+//   return {
+//     props: {
+//       testId: testId === "food" ? "food" : "knowledge",
+//       defaultScore: decryptedScore
+//     },
+//   }
+// }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const testId = context?.params?.testId || ""
   const scoreParam = context?.params?.s || ""
 
